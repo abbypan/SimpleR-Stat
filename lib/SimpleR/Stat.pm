bb0b57bf2014e45ef30d1851eb20250848a1b6f5
Abby Pan <abbypan@gmail.com>
2013-12-02 11:55:38 +0800

    sum, median, mean

--- lib/SimpleR/Stat.pm
+++ lib/SimpleR/Stat.pm
@@ -3,7 +3,10 @@ package SimpleR::Stat;
 
 require Exporter;
 @ISA    = qw(Exporter);
-@EXPORT = qw( calc_rate calc_rate_arrayref format_percent calc_compare_rate);
+@EXPORT = qw( calc_rate calc_rate_arrayref format_percent 
+calc_compare_rate
+sum_arrayref mean_arrayref median_arrayref
+);
 
 use strict;
 use warnings;
@@ -24,14 +27,11 @@ sub calc_rate_arrayref {
     my ($r, %opt) = @_;
     my $fields = $opt{calc_fields} || [ 0 .. $#$r ];
 
-    my $num=0;
-    for my $i (@$fields){
-        $r->[$i] ||=0;
-        $num+=$r->[$i];
-    }
-
+    my $num = sum_arrayref([ @{$r}{@$fields} ] );
     push @$r, $num;
-    for my $i (@$fields){
+
+    for my $i (@{$opt{calc_fields}}){
+        $r->[$i] ||= 0;
         my $x = calc_rate($r->[$i], $num);
         $x = $opt{rate_sub}->($x) if(exists $opt{rate_sub});
         push @$r, $x;
@@ -59,5 +59,30 @@ sub calc_compare_rate {
     $diff / $old;
     return wantarray ? ($rate, $diff) : $rate;
 } ## end sub calc_compare_rate
+
+sub sum_arrayref {
+    my ($r) = @_;
+    my $num=0;
+    $num += $_ || 0 for @$r;
+    return $num;
+}
+
+sub mean_arrayref {
+    my ($data) = @_;
+    my $n = scalar(@$data);
+    return calc_rate( sum_arrayref($data), $n );
+}
+
+sub median_arrayref {
+    my ($data) = @_;
+    my $n = $#$data;
+
+    my @d = sort { $a <=> $b } @$data;
+
+    return $d[ $n / 2 ] if ( $n % 2 == 0 );
+
+    my $m = ( $n - 1 ) / 2;
+    return ( $d[$m] + $d[ $m + 1 ] ) / 2;
+}
 
 1;
